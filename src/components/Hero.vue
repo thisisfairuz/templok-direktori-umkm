18438310c6a4e9a35389f006bbdfc6f581318d63
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2025-11-14 17:01:33 +0700

    add Hero component and update App.vue; include styles and assets for improved UI

---
+++ src/components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="w-full h-full flex flex-col justify-center items-center">
+    <div class="flex space-x-1">
+      <img
+        src="/src/assets/bunga-green.svg"
+        alt="Bunga Hijau"
+        class="w-36 h-36 -mt-3 -ms-3 animate-spin-slow"
+      />
+      <div class="flex justify-center items-center flex-col">
+        <div class="mb-7">
+          <div
+            class="league-spartan-black font-black mb-3 justify-center items-center flex flex-col"
+          >
+            <p class="text-white text-9xl">TEMPAT</p>
+            <p class="text-green-templok text-9xl">LOKAL</p>
+          </div>
+          <p class="text-white font-bold text-4xl font-montserrat">
+            Biar yang kecil makin dikenal
+          </p>
+        </div>
+        <button
+          class="atkinson-hyperlegible-bold text-blue-templok bg-green-templok px-12 py-3 rounded-full text-5xl"
+        >
+          Explore More!
+        </button>
+      </div>
+      <img
+        src="/src/assets/bunga-white.svg"
+        alt="Bunga Putih"
+        class="w-36 h-36 mt-28 -ms-5 animate-spin-slow-reverse"
+      />
+    </div>
+  </section>
+</template>
