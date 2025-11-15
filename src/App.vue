f2ca67e4dd54f6db3a663f3ee29a9245167765e8
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2025-11-15 13:33:31 +0700

    feat: Refactor App.vue and add new components
    - Updated App.vue to include Header and About components.
    - Removed unused logo links and styles from App.vue.
    - Added About.vue component with content and SVG graphics.
    - Created Header.vue component with navigation links and logo.
    - Adjusted Hero.vue for consistent layout.
    - Added new assets: img-about.png, pill-about.png, wave-border.svg, and logo-templok.svg.
    - Updated style.css with new styles for About section and typography.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
 import Hero from "./components/Hero.vue";
+import Header from "./components/Header.vue";
+import About from "./components/About.vue";
 </script>
 
 <template>
   <div class="absolute inset-0 h-full w-full grid-templok">
-    <!-- <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
+    <Header />
     <Hero />
+    <About />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
